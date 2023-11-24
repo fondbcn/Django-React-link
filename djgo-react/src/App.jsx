3b37aa451e1092336b821e7efeb4cba3a1d25b9b
fondbcn <wakilobarca11@gmail.com>
2023-11-24 09:12:59 +0100

    react initializations

---
+++ djgo-react/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState,useEffect,useRef,useHistory,useMemo } from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default App
