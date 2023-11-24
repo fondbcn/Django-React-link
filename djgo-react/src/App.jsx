6bccb941e023dea2d7c368714f9ed651c21b1320
fondbcn <wakilobarca11@gmail.com>
2023-11-24 11:01:16 +0100

    introducing the funcs

--- djgo-react/src/App.jsx
+++ djgo-react/src/App.jsx
@@ -1,13 +1,44 @@
-import { useState,useEffect,useRef,useHistory,useMemo } from 'react'
+import React,{ useState,useEffect,useRef,useHistory,useMemo } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css'
 
-function App() {
+const tasks=[
+  {id:1,
+  title:"one",
+  description:"nothing task",
+  completed:true},
+  {id:2,
+  title:"two",
+  description:"first-sec task",
+  completed:true},
+  {id:3,
+  title:"three",
+  description:"future task",
+  completed:false},
+]
 
-  return (
-    <>
+function App() {
+  const[viewCompleted,setViewCompleted]=useState(false)
+  const[taskList,setTaskList]=useState(tasks)
+  let tabGen=()=>{
+      return(
+        <div className="my-5 tab-list">
+          <span className={viewCompleted ? "active" : ""} onClick={()=>setViewCompleted(true)}>Completed</span>
+          <span className={viewCompleted ? "" : "active"} onClick={()=>setViewCompleted(false)}>Incompleted</span>
+        </div>
+      )
+  }
+  let items=()=>{
+      const newItems=taskList.filter(item => item.copmleted==viewCompleted)
       
-    </>
+  }
+  
+  return (
+      <main className="content">
+        <h1 className="text-black text-uppercase text-center my-4">Task Manager</h1>
+        <div className="row ">
+          
+      </main>
   )
 }
 
